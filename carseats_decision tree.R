9883b40c4862928da4a357713680d7de5f6b574f
aarthiayyanar <aarthi.p18001@iimtrichy.ac.in>
2018-08-21 23:28:55 +0530

    21-8

--- carseats_decision tree.R
+++ carseats_decision tree.R
@@ -24,7 +24,10 @@ rpart.plot(prunetree)
 
 #improve the plot
 rpart.plot(prunetree, nn=T, cex=.8, type=4)
-
+prunetree2= prune(fit,cp=.014)
+library(dplyr)
+(testdata=sample_n(data,2))
+predict(prunetree2,newdata=testdata,type='class')
 
 
 #https://www.datacamp.com/community/tutorials/decision-trees-R
---
+++ kmeans.R
@@ -0,0 +1,16 @@
+#clustering
+
+marks1=c(3,1,4,5,9,2,6)
+marks= data.frame(marks1)
+length(marks1)
+plot(marks1)
+
+k1=kmeans(marks,centers=2)
+k1
+cbind(marks,k1$cluster)
+marks2=sample(1:10,size=7)
+newmarks = data.frame(marks1,marks2)
+
+k2= kmeans(newmarks,centers = 3)
+k2
+cbind(newmarks,k2$cluster)
